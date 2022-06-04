defa4e6c4630c7bbbb0facc1fa2eec411fb3a88c
FinDosky <106819073+FinDosky@users.noreply.github.com>
2022-06-04 15:33:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
     btnNo: 'Không có đâu anh à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    reply: 'Yêu thì yêu mà không yêu thì cũng yêu :)))))',
+    mess: 'Anh biết mà 🥰. Yêu bé nhiều 😘😘',
     messDesc: 'Chiều nay 5h anh qua đón nhé công chúa.',
     btnAccept: 'Kê <3',
     messLink: 'https://www.facebook.com/profile.php?id=100028347164722' 
